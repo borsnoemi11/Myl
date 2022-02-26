935697271cd177720c5188d04966b2bf6c04e2fc
Noemi <borsnoemi11@gmail.com>
2022-02-26 12:47:44 +0100

    E01 END code organization, namespace, folder

---
+++ mc/CodeAnalysis/SyntaxNode.cs
@@ -0,0 +1,19 @@
+namespace Minsk.CodeAnalysis
+{
+    abstract class SyntaxNode
+    {
+        // 1 + 2 * 3
+        //
+        // this will be a tree
+        // 
+        //         +
+        //        / \
+        //       1   *
+        //          / \
+        //         2   3
+
+        public abstract SyntaxKind Kind { get; }
+
+        public abstract IEnumerable<SyntaxNode> GetChildren();
+    }
+}
--- mc/Program.cs
+++ mc/Program.cs
@@ -1,4 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Minsk.CodeAnalysis;
 
 bool showTree = false;
 while (true)
