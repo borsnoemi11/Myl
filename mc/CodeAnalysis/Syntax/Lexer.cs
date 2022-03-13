055c2e8c9385497f2f8b8444e3262eabb6a31687
Noemi <borsnoemi11@gmail.com>
2022-03-13 15:56:33 +0100

    E02 01:30:42 Add support for !, && and ||

--- mc/CodeAnalysis/Syntax/Lexer.cs
+++ mc/CodeAnalysis/Syntax/Lexer.cs
@@ -13,17 +13,8 @@ namespace Minsk.CodeAnalysis.Syntax
 
         public IEnumerable<string> Diagnostics => _diagnostics;
 
-        private char Current
-        {
-            get
-            {
-                if (_position >= _text.Length)
-                {
-                    return '\0';
-                }
-                return _text[_position];
-            }
-        }
+        private char Current => Peek(0);
+        private char Lookahead => Peek(1);
 
         public SyntaxToken Lex()
         {
@@ -93,10 +84,31 @@ namespace Minsk.CodeAnalysis.Syntax
                     return new SyntaxToken(SyntaxKind.OpenParenthesisToken, _position++, "(", null);
                 case ')':
                     return new SyntaxToken(SyntaxKind.CloseParenthesisToken, _position++, ")", null);
+                case '!':
+                    return new SyntaxToken(SyntaxKind.BangToken, _position++, "!", null);
+                case '&':
+                    if (Lookahead == '&')
+                        return new SyntaxToken(SyntaxKind.AmpersandAmpersandToken, _position += 2, "&&", null);
+                    break;
+                case '|':
+                    if (Lookahead == '|')
+                        return new SyntaxToken(SyntaxKind.PipePipeToken, _position += 2, "||", null);
+                    break;
+
             }
 
             _diagnostics.Add($"ERROR: bad character input: '{Current}'");
             return new SyntaxToken(SyntaxKind.BadToken, _position++, _text.Substring(_position - 1, 1), null);
+        }
+
+        private char Peek(int offset)
+        {
+            var index = _position + offset;
+            if (index >= _text.Length)
+            {
+                return '\0';
+            }
+            return _text[index];
         }
 
         private void Next() => _position++;
